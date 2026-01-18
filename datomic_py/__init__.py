e41aa8235ebb4d8cc958e98b343c71d77d1b9981
Julien Ambrosio <julienbreno.nave@gmail.com>
2026-01-18 12:08:29 -0300

    feat: add schema value type constants
    Expose Datomic value type constants to simplify schema definitions.

--- datomic_py/__init__.py
+++ datomic_py/__init__.py
@@ -10,33 +10,63 @@ from datomic_py.exceptions import (
     EDNParseError,
 )
 from datomic_py.schema import (
+    BIGDEC,
+    BIGINT,
     BOOLEAN,
+    BYTES,
+    DOUBLE,
+    FLOAT,
     IDENTITY,
+    INSTANT,
+    KEYWORD,
+    LONG,
     MANY,
     ONE,
+    REF,
     STRING,
+    URI,
+    UUID,
     VALUE,
     Attribute,
     Schema,
 )
 
 __all__ = [
+    # Clients
     "AsyncDatabase",
     "AsyncDatomic",
     "Database",
     "Datomic",
+    # EDN
     "edn_loads",
+    # Schema helpers
     "Attribute",
     "Schema",
+    # Cardinality
     "ONE",
     "MANY",
+    # Value types
     "STRING",
     "BOOLEAN",
+    "LONG",
+    "BIGINT",
+    "FLOAT",
+    "DOUBLE",
+    "BIGDEC",
+    "INSTANT",
+    "UUID",
+    "URI",
+    "KEYWORD",
+    "REF",
+    "BYTES",
+    # Uniqueness
     "IDENTITY",
     "VALUE",
+    # Exceptions
     "DatomicPyError",
     "DatomicClientError",
     "DatomicConnectionError",
     "EDNParseError",
+    # Version
     "__version__",
 ]
